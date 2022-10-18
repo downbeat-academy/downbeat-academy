f9248c988ef9150e98f69f3d76b9e05a7f469397
Jory Tindall <jory.tindall@hashicorp.com>
2022-10-17 22:57:56 -0700

    Reinstalled Vite project, added fonts

--- packages/cadence-core/src/components/button/Button.tsx
+++ packages/cadence-core/src/components/button/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export interface ButtonProps {
+interface ButtonProps {
     children: React.ReactNode,
 }
 
--- packages/cadence-core/vite.config.ts
+++ packages/cadence-core/vite.config.ts
@@ -1,37 +1,30 @@
-import { resolve } from 'node:path'
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
-import EsLint from 'vite-plugin-linter'
-import tsConfigPaths from 'vite-tsconfig-paths'
-const { EsLinter, linterPlugin } = EsLint
-import * as packageJson from './package.json'
-// https://vitejs.dev/config/
-export default defineConfig((configEnv) => ({
-  plugins: [
-    react(),
-    tsConfigPaths(),
-    linterPlugin({
-      include: ['./src/**/*.{ts,tsx}'],
-      linters: [new EsLinter({ configEnv })],
-    }),
-    dts({
-      include: ['src/components/'],
-    }),
-  ],
-  build: {
-    lib: {
-      entry: resolve('src', 'components/index.ts'),
-      name: 'CadenceCore',
-      formats: ['es', 'umd'],
-      fileName: (format) => `cadence-core.${format}.js`,
+export default defineConfig({
+    plugins: [
+        react(),
+        dts({
+            insertTypesEntry: true,
+        }),
+    ],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/components/index.ts'),
+            name: 'CadenceCore',
+            formats: ['es', 'umd'],
+            fileName: (format) => `cadence-core.${format}.js`,
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                },
+            },
+        },
     },
-    // rollupOptions: {
-    //   external: [...Object.keys(packageJson)],
-    // },
-    rollupOptions: {
-      external: [...Object.keys(packageJson.peerDependencies)],
-    },
-  },
-}))
+});
