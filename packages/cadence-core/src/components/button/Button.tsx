42d643287b7451903357010beface976e87a7fe6
Jory Tindall <jory.tindall@hashicorp.com>
2022-10-18 22:22:18 -0700

    Updating button component styling

--- packages/cadence-core/src/components/button/Button.tsx
+++ packages/cadence-core/src/components/button/Button.tsx
@@ -4,10 +4,21 @@ import styles from '../../styles/components/button/Button.module.scss'
 
 interface ButtonProps {
     children: React.ReactNode,
+    variant?: 'primary' | 'secondary' | 'tertiary' | 'ghost' | 'destructive',
+    size?: 'large' | 'default' | 'small' | 'extra-small',
 }
 
-export const Button = ({ children }: ButtonProps) => {
+export const Button = ({ 
+    children,
+    variant = 'primary',
+    size = 'default',
+}: ButtonProps) => {
+    const classes = getClasses([
+        styles[variant],
+        styles[size]
+    ])
+
     return (
-        <button className={styles.button}>{children}</button>
+        <button className={classes}>{children}</button>
     )
 }
