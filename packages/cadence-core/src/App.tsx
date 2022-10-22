54e70ac8eb5126843f198104c273b970ecde46e4
Jory Tindall <jory.tindall@hashicorp.com>
2022-10-21 19:22:27 -0700

    Added ButtonSet component

--- packages/cadence-core/src/App.tsx
+++ packages/cadence-core/src/App.tsx
@@ -1,99 +1,21 @@
 import React from 'react'
-import { Button } from './components'
+import { Button, ButtonSet } from './components'
 
 function App() {
 
   return (
     <div>
       <h1>Testing playground</h1>
-      <div>
-        <Button 
-          text='Primary button'
-          variant='primary'
-          size='large'
-        />
-        <Button
-          text='Primary button'
-          variant='primary'
-          size='default'
-        />
-        <Button
-          text='Primary button'
-          variant='primary'
-          size='small'
-        />
-        <Button
-          text='Primary button'
-          variant='primary'
-          size='x-small'
-        />
-      </div>
-      <div>
-        <Button
-          text='Primary button'
-          variant='secondary'
-          size='large'
-        />
-        <Button
-          text='Primary button'
-          variant='secondary'
-          size='default'
-        />
-        <Button
-          text='Primary button'
-          variant='secondary'
-          size='small'
-        />
-        <Button
-          text='Primary button'
-          variant='secondary'
-          size='x-small'
-        />
-      </div>
-      <div>
-        <Button
-          text='Primary button'
-          variant='tertiary'
-          size='large'
-        />
-        <Button
-          text='Primary button'
-          variant='tertiary'
-          size='default'
-        />
-        <Button
-          text='Primary button'
-          variant='tertiary'
-          size='small'
-        />
-        <Button
-          text='Primary button'
-          variant='tertiary'
-          size='x-small'
-        />
-      </div>
-      <div>
-        <Button
-          text='Primary button'
-          variant='destructive'
-          size='large'
-        />
-        <Button
-          text='Primary button'
-          variant='destructive'
-          size='default'
-        />
-        <Button
-          text='Primary button'
-          variant='destructive'
-          size='small'
-        />
-        <Button
-          text='Primary button'
-          variant='destructive'
-          size='x-small'
-        />
-      </div>
+      <ButtonSet justify='space-between'>
+        <ButtonSet
+          justify='start'
+          gap='default'
+        >
+          <Button text='Primary' variant='primary' size='default' />
+          <Button text='Cancel' variant='secondary' size='default' />
+        </ButtonSet>
+        <Button text='See more' variant='tertiary' size='default' />
+      </ButtonSet>
     </div>
   )
 }
--- packages/cadence-core/src/components/index.ts
+++ packages/cadence-core/src/components/index.ts
@@ -1,1 +1,2 @@
 export { Button } from './button'
+export { ButtonSet } from './buttonSet'
