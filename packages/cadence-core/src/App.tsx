8a8ceb1785911205e99ea17dd610b058fbc4d9a6
Jory Tindall <jory.tindall@hashicorp.com>
2022-10-24 18:18:22 -0700

    Added different variants for badge component'

--- packages/cadence-core/src/App.tsx
+++ packages/cadence-core/src/App.tsx
@@ -6,11 +6,306 @@ function App() {
   return (
     <div>
       <h1>Testing playground</h1>
-      <Badge
-        text='Badge text'
-        type='informational'
-        size='default'
-      />
+      <div>
+        <Badge
+          text='Badge text'
+          type='neutral'
+          size='small'
+          style='fill'
+        />
+        <Badge
+          text='Badge text'
+          type='neutral'
+          size='default'
+          style='fill'
+        />
+        <Badge
+          text='Badge text'
+          type='neutral'
+          size='large'
+          style='fill'
+        />
+      </div>
+      <div>
+        <Badge
+          text='Badge text'
+          type='informational'
+          size='small'
+          style='fill'
+        />
+        <Badge
+          text='Badge text'
+          type='informational'
+          size='default'
+          style='fill'
+        />
+        <Badge
+          text='Badge text'
+          type='informational'
+          size='large'
+          style='fill'
+        />
+      </div>
+      <div>
+        <Badge
+          text='Badge text'
+          type='positive'
+          size='small'
+          style='fill'
+        />
+        <Badge
+          text='Badge text'
+          type='positive'
+          size='default'
+          style='fill'
+        />
+        <Badge
+          text='Badge text'
+          type='positive'
+          size='large'
+          style='fill'
+        />
+      </div>
+      <div>
+        <Badge
+          text='Badge text'
+          type='warning'
+          size='small'
+          style='fill'
+        />
+        <Badge
+          text='Badge text'
+          type='warning'
+          size='default'
+          style='fill'
+        />
+        <Badge
+          text='Badge text'
+          type='warning'
+          size='large'
+          style='fill'
+        />
+      </div>
+      <div>
+        <Badge
+          text='Badge text'
+          type='critical'
+          size='small'
+          style='fill'
+        />
+        <Badge
+          text='Badge text'
+          type='critical'
+          size='default'
+          style='fill'
+        />
+        <Badge
+          text='Badge text'
+          type='critical'
+          size='large'
+          style='fill'
+        />
+      </div>
+      <div>
+        <Badge
+          text='Badge text'
+          type='neutral'
+          size='small'
+          style='outline'
+        />
+        <Badge
+          text='Badge text'
+          type='neutral'
+          size='default'
+          style='outline'
+        />
+        <Badge
+          text='Badge text'
+          type='neutral'
+          size='large'
+          style='outline'
+        />
+      </div>
+      <div>
+        <Badge
+          text='Badge text'
+          type='informational'
+          size='small'
+          style='outline'
+        />
+        <Badge
+          text='Badge text'
+          type='informational'
+          size='default'
+          style='outline'
+        />
+        <Badge
+          text='Badge text'
+          type='informational'
+          size='large'
+          style='outline'
+        />
+      </div>
+      <div>
+        <Badge
+          text='Badge text'
+          type='positive'
+          size='small'
+          style='outline'
+        />
+        <Badge
+          text='Badge text'
+          type='positive'
+          size='default'
+          style='outline'
+        />
+        <Badge
+          text='Badge text'
+          type='positive'
+          size='large'
+          style='outline'
+        />
+      </div>
+      <div>
+        <Badge
+          text='Badge text'
+          type='warning'
+          size='small'
+          style='outline'
+        />
+        <Badge
+          text='Badge text'
+          type='warning'
+          size='default'
+          style='outline'
+        />
+        <Badge
+          text='Badge text'
+          type='warning'
+          size='large'
+          style='outline'
+        />
+      </div>
+      <div>
+        <Badge
+          text='Badge text'
+          type='critical'
+          size='small'
+          style='outline'
+        />
+        <Badge
+          text='Badge text'
+          type='critical'
+          size='default'
+          style='outline'
+        />
+        <Badge
+          text='Badge text'
+          type='critical'
+          size='large'
+          style='outline'
+        />
+      </div>
+      <div>
+        <Badge
+          text='Badge text'
+          type='neutral'
+          size='small'
+          style='inverse'
+        />
+        <Badge
+          text='Badge text'
+          type='neutral'
+          size='default'
+          style='inverse'
+        />
+        <Badge
+          text='Badge text'
+          type='neutral'
+          size='large'
+          style='inverse'
+        />
+      </div>
+      <div>
+        <Badge
+          text='Badge text'
+          type='informational'
+          size='small'
+          style='inverse'
+        />
+        <Badge
+          text='Badge text'
+          type='informational'
+          size='default'
+          style='inverse'
+        />
+        <Badge
+          text='Badge text'
+          type='informational'
+          size='large'
+          style='inverse'
+        />
+      </div>
+      <div>
+        <Badge
+          text='Badge text'
+          type='positive'
+          size='small'
+          style='inverse'
+        />
+        <Badge
+          text='Badge text'
+          type='positive'
+          size='default'
+          style='inverse'
+        />
+        <Badge
+          text='Badge text'
+          type='positive'
+          size='large'
+          style='inverse'
+        />
+      </div>
+      <div>
+        <Badge
+          text='Badge text'
+          type='warning'
+          size='small'
+          style='inverse'
+        />
+        <Badge
+          text='Badge text'
+          type='warning'
+          size='default'
+          style='inverse'
+        />
+        <Badge
+          text='Badge text'
+          type='warning'
+          size='large'
+          style='inverse'
+        />
+      </div>
+      <div>
+        <Badge
+          text='Badge text'
+          type='critical'
+          size='small'
+          style='inverse'
+        />
+        <Badge
+          text='Badge text'
+          type='critical'
+          size='default'
+          style='inverse'
+        />
+        <Badge
+          text='Badge text'
+          type='critical'
+          size='large'
+          style='inverse'
+        />
+      </div>
     </div>
   )
 }
