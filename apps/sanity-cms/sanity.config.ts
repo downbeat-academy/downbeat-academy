e85dce8899450eec36992b810a3f1fade28bfd2c
Jory Tindall <jory.tindall@hashicorp.com>
2022-12-23 13:19:15 -0800

    Initial config of Sanity Studio v3

--- apps/sanity-cms/sanity.config.ts
+++ apps/sanity-cms/sanity.config.ts
@@ -1,16 +1,57 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { visionTool } from '@sanity/vision'
+import {
+  dashboardTool,
+  projectUsersWidget,
+  projectInfoWidget,
+} from '@sanity/dashboard'
+import { documentListWidget } from 'sanity-plugin-dashboard-widget-document-list'
+import { schemaTypes } from './schemas'
 
 export default defineConfig({
-  name: 'default',
+  name: 'downbeat-academy',
   title: 'Downbeat Academy',
 
   projectId: 'v5w3t766',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    dashboardTool({
+      widgets: [
+        projectInfoWidget({
+          layout: 'medium',
+        }),
+        projectUsersWidget({
+          layout: 'medium',
+        }),
+        documentListWidget({
+          layout: 'large',
+          showCreateButton: true,
+          limit: 5,
+          title: 'Recent articles',
+          types: [
+            'article',
+          ],
+        }),
+        documentListWidget({
+          layout: 'large',
+          limit: 10,
+          title: 'Recently updated docuemnts',
+          types: [
+            'course',
+            'curriculum',
+            'lesson',
+            'page',
+            'podcast',
+            'resource',
+          ]
+        })
+      ]
+    }),
+    deskTool(),
+    visionTool(),
+  ],
 
   schema: {
     types: schemaTypes,
