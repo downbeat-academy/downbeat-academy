3473bcd0a2e8df417384e1349be198af8d19fd69
Jory Tindall <jory.tindall@hashicorp.com>
2022-12-26 14:18:13 -0800

    Added hook for Fathom analytics

--- apps/www/pages/_app.tsx
+++ apps/www/pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app'
+import { useFathom } from '../hooks/useFathom'
 
 export default function App({ Component, pageProps }: AppProps) {
+	useFathom()
 	return <Component {...pageProps} />
 }
