6f7dea44be1e05ead29d5cf6e34e03b0d2532949
Jory Tindall <jory.tindall@hashicorp.com>
2022-12-26 14:21:13 -0800

    Added absolute import support

--- apps/www/pages/_app.tsx
+++ apps/www/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app'
-import { useFathom } from '../hooks/useFathom'
+import { useFathom } from '@hooks/useFathom'
 
 export default function App({ Component, pageProps }: AppProps) {
 	useFathom()
