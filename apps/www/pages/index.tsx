b1b90f461a973ee7df93cefbd716c7f494cc6d20
Jory Tindall <jory.tindall@hashicorp.com>
2022-12-31 13:18:24 -0800

    Added site metadata and seo component

--- apps/www/pages/index.tsx
+++ apps/www/pages/index.tsx
@@ -1,6 +1,13 @@
+import { Seo } from '@components/meta'
+
 export default function Home() {
 	return (
 		<>
+			<Seo
+				title='Top resources for advancing musicians, students, and educators'
+				description='Welcome to the home of Downbeat Academy, with top courses and resources by leading professionals and educators.'
+				url=''
+			/>
 			<p>App</p>
 		</>
 	)
