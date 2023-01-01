26000f53d94a356dbc92d7fe99d50ac9c29b2d17
Jory Tindall <jory.tindall@hashicorp.com>
2022-12-31 17:30:36 -0800

    Added placeholder files and documents

---
+++ apps/www/pages/500.tsx
@@ -0,0 +1,13 @@
+import { Seo } from '@components/meta'
+
+export default function ServerErrorPage() {
+  return (
+    <>
+      <Seo
+        title='500: Server error'
+        noindex={true}
+      />
+      <p>500</p>
+    </>
+  )
+}
---
+++ apps/www/pages/[slug].tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <p>This is a page</p>
+  )
+}
