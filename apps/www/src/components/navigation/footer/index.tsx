b5303f6d3cd77e4e7a43099257e646824ad2f946
Jory Tindall <jory.tindall@hashicorp.com>
2023-01-07 18:43:42 -0800

    Cleaning up styling

--- apps/www/src/components/navigation/footer/index.tsx
+++ apps/www/src/components/navigation/footer/index.tsx
@@ -4,9 +4,8 @@ import {
   LogoSymbol,
   Flex,
 } from 'cadence-core'
-import { linkResolver } from '@utils/linkResolver'
 import { socialLinks, siteLinks } from './FooterLinks'
-import s from './footer.module.scss'
+import s from '@styles/components/navigation/footer.module.scss'
 
 const Footer = ({ }) => {
 
@@ -16,19 +15,20 @@ const Footer = ({ }) => {
         href={socialLink.url}
         rel='noreferrer noopener'
         key={socialLink.name}
+        className={s.footerLink}
       >{`${socialLink.title}`}</a>
     )
   })
 
-  console.log(socialLinks[0].icon)
-
   const getSiteLinks = siteLinks.map(category => {
     return (
-      <Flex direction='column' key={category.name}>
+      <Flex direction='column' key={category.name} gap='small'>
         <Text tag='p'><strong>{category.title}</strong></Text>
-        {category.links.map(link => {
-          return <Link href={link.url} key={link.name}>{link.title}</Link>
-        })}
+        <Flex direction='column' gap='small'>
+          {category.links.map(link => {
+            return <Link href={link.url} key={link.name} className={s.footerLink}>{link.title}</Link>
+          })}
+        </Flex>
       </Flex>
     )
   })
@@ -38,10 +38,12 @@ const Footer = ({ }) => {
       <div className={s.content}>
         <Flex direction='column'>
           <LogoSymbol color='secondary' width={80} />
-          {getSocialLinks}
-        </Flex>
-        <Flex direction='row'>
-          {getSiteLinks}
+          <Flex direction='row' gap='x-large'>
+            <Flex direction='column' gap='small'>
+              {getSocialLinks}
+            </Flex>
+            {getSiteLinks}
+          </Flex>
         </Flex>
       </div>
     </footer>
