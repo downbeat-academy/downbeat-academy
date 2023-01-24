4b6cba621cf7499e2b21dbf97deadeef2e4fcc4c
Jory Tindall <jory.tindall@hashicorp.com>
2023-01-24 12:00:01 -0800

    Add box shadow on scroll to navigation

--- apps/www/src/components/navigation/header/index.tsx
+++ apps/www/src/components/navigation/header/index.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import classnames from 'classnames'
 import { Flex, LogoLockup } from 'cadence-core'
 import s from '@styles/components/navigation/header/header.module.scss'
 
@@ -11,6 +12,7 @@ import { Toggle } from './Toggle'
 const Header = () => {
   const route = useRouter().asPath;
   const [navToggled, setNavToggled] = useState(false)
+  const [scroll, setScroll] = useState(false)
 
   const handleNavToggle = () => {
     setNavToggled(!navToggled);
@@ -18,8 +20,21 @@ const Header = () => {
 
   useEffect(() => setNavToggled(false), [route])
 
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', () => {
+        setScroll(window.pageYOffset > 100);
+      });
+    }
+  }, []);
+
+  const classes = classnames(
+    s.wrapper,
+    [scroll ? s.scroll : ''],
+  )
+
   return (
-    <header className={s.wrapper}>
+    <header className={classes}>
       <Secondary />
       <nav className={s.navbar}>
         <div className={s.navbarContent}>
