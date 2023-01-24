d9f8ee77a7eb92dc6e268da1e6564120242a6276
Jory Tindall <jory.tindall@hashicorp.com>
2023-01-23 23:40:24 -0800

    Fixed sanity client

--- apps/www/src/lib/sanity.client.ts
+++ apps/www/src/lib/sanity.client.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'next-sanity'
 
-// export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-// export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
-// export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION
 
 export const sanityClient = createClient({
 	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
