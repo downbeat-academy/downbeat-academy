10e10426c79f8088e37209cd2f6a850956809a22
Jory Tindall <jory.tindall@hashicorp.com>
2023-01-28 12:51:36 -0800

    Updating rich text

--- apps/www/src/components/content-modules/rich-text/components.tsx
+++ apps/www/src/components/content-modules/rich-text/components.tsx
@@ -1,4 +1,4 @@
-import { Text } from 'cadence-core'
+import { Text, Blockquote, List } from 'cadence-core'
 
 const components = {
   block: {
@@ -9,11 +9,29 @@ const components = {
     h5: ({ children }) => <Text tag='h5' size='2x-large' type='expressive' category='headline' color='primary'>{children}</Text>,
     h6: ({ children }) => <Text tag='h6' size='x-large' type='expressive' category='headline' color='primary'>{children}</Text>,
     normal: ({ children }) => <Text tag='p' size='large' type='expressive' category='body' color='primary'>{children}</Text>,
-    blockquote: ({ children }) => <blockquote>{children}</blockquote>
+    blockquote: ({ children }) => <Blockquote>{children}</Blockquote>
   },
-  list: {},
-  listItem: {},
-  types: {},
+  list: {
+    bullet: ({ children }) => <List listType='ul' context='expressive'>{children}</List>,
+    number: ({ children }) => <List listType='ol' context='expressive'>{children}</List>,
+  },
+  listItem: {
+    bullet: ({ children }) => <li>{children}</li>,
+    number: ({ children }) => <li>{children}</li>,
+  },
+  types: {
+    blockquote: ({ value }) => {
+      const { attribution, source, quote } = value
+      return (
+        <Blockquote attribution={attribution} source={source} quote={quote} />
+      )
+    },
+    inlineChord: ({ value }) => { },
+    inlineMusicText: ({ value }) => { },
+    mainImage: ({ value }) => { },
+    musicNotation: ({ value }) => { },
+    video: ({ value }) => <p>Video currently isn&apos;t supported.</p>
+  },
   marks: {},
 }
 
