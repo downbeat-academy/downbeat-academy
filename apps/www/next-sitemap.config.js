3864c7e5c99d7fcf1126b5fbbbdc6b46747bf4d7
Jory Tindall <jory.tindall@hashicorp.com>
2023-02-12 11:27:41 -0800

    Added basic sitemap config

---
+++ apps/www/next-sitemap.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteUrl: 'https://downbeatacademy.com',
+  generateRobotosTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      }
+    ]
+  }
+}
