b69be24881d5ef7fa023bbd9051b9fbc1f3a5d23
Jory Tindall <jory.tindall@hashicorp.com>
2023-03-10 11:19:33 -0800

    Generating components and setting up react output

---
+++ packages/cadence-react/dist/components/stencil-generated/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+export declare const CdsButton: import("react").ForwardRefExoticComponent<Pick<any, string | number | symbol> & import("react").RefAttributes<any>>;
+export declare const MyComponent: import("react").ForwardRefExoticComponent<Pick<any, string | number | symbol> & import("react").RefAttributes<any>>;
--- packages/cadence-react/src/components/stencil-generated/index.ts
+++ packages/cadence-react/src/components/stencil-generated/index.ts
@@ -5,7 +5,8 @@ import { createReactComponent } from './react-component-lib';
 
 import type { JSX } from 'cadence-web';
 
+import { defineCustomElements } from 'cadence-web/loader';
 
-
+defineCustomElements();
 export const CdsButton = /*@__PURE__*/createReactComponent<JSX.CdsButton, HTMLCdsButtonElement>('cds-button');
 export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
