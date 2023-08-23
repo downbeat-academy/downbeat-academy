e721990dc635549951e6bcb7eef2557448be972a
Jory Tindall <jory.tindall@hashicorp.com>
2023-08-22 23:02:33 -0700

    Testing

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { ContentWrapper, Content } from '@components/content-wrapper'
 import '@styles/index.scss'
 
 import { Text } from '@components/text'
+import { Link } from '@components/link'
 
 export default function RootLayout({
   children,
@@ -17,17 +18,7 @@ export default function RootLayout({
           <HeaderNavigation />
           <ContentWrapper>
             <Content isFullBleed>
-              <Text tag='h1' color='brand' type='expressive-headline' size='mega' collapse>Mega headline</Text>
-              <Text tag='h1' color='brand' type='expressive-headline' size='h1' collapse>Headline 1</Text>
-              <Text tag='h2' color='brand' type='expressive-headline' size='h2' collapse>Headline 2</Text>
-              <Text tag='h3' color='brand' type='expressive-headline' size='h3' collapse>Headline 3</Text>
-              <Text tag='h4' color='brand' type='expressive-headline' size='h4' collapse>Headline 4</Text>
-              <Text tag='h5' color='brand' type='expressive-headline' size='h5' collapse>Headline 5</Text>
-              <Text tag='h6' color='brand' type='expressive-headline' size='h6' collapse>Headline 6</Text>
-              <Text tag='p' color='primary' type='expressive-body' size='body-large'>Lorem ipsum dolor sit amet.</Text>
-              <Text tag='p' color='primary' type='expressive-body' size='body-base'>Lorem ipsum dolor sit amet.</Text>
-              <Text tag='p' color='primary' type='expressive-body' size='body-small'>Lorem ipsum dolor sit amet.</Text>
-              <Text tag='p' color='primary' type='expressive-body' size='body-x-small'>Lorem ipsum dolor sit amet.</Text>
+              {children}
             </Content>
           </ContentWrapper>
           <Footer />
