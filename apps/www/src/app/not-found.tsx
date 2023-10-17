e73e9703c46406189a79a5293b174dc1aadbc446
Jory Tindall <jory.tindall@hashicorp.com>
2023-10-16 23:20:04 -0700

    Added basic catch-all not found page

---
+++ apps/www/src/app/not-found.tsx
@@ -0,0 +1,42 @@
+import { Flex } from "@components/flex";
+import { Link } from "@components/link";
+import { SectionContainer } from "@components/section-container";
+import { SectionTitle } from '@components/section-title'
+import { Text } from '@components/text'
+
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: '404: Sorry, couldn&apos;t find that page'
+}
+
+export default function NotFound() {
+  return (
+    <SectionContainer>
+      <SectionTitle
+        background='critical'
+        title={
+          <Text
+            type='expressive-headline'
+            size='h1'
+            tag='h1'
+            color='high-contrast'
+            collapse
+          >404 :(</Text>
+        }
+        subtitle={
+          <Text
+            type='expressive-body'
+            size='body-base'
+            tag='p'
+            color='high-contrast'
+            collapse
+          >Sorry, couldn&apos;t find that page</Text>
+        }
+      />
+      <Flex>
+        <Text type='expressive-body' size='body-base' tag='p' color='primary'><Link href='/'>Back to the home page.</Link></Text>
+      </Flex>
+    </SectionContainer>
+  )
+}
