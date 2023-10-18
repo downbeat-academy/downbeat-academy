860010df6092055d3bb2ccce101f8ff46041063e
Jory Tindall <jory.tindall@hashicorp.com>
2023-10-17 23:38:51 -0700

    Removed cadence-react

--- packages/cadence-react/src/components/buttonSet/types.ts
+++
@@ -1,9 +0,0 @@
-interface ButtonSetProps {
-	children: React.ReactNode
-	className?: string
-	direction?: 'row' | 'column'
-	justify?: 'start' | 'end' | 'space-between'
-	gap?: 'none' | 'x-small' | 'small' | 'default' | 'large'
-}
-
-export type { ButtonSetProps }
--- packages/cadence-react/src/components/link/types.ts
+++
@@ -1,8 +0,0 @@
-interface LinkProps {
-	children: React.ReactNode
-	type?: 'interactive' | 'primary' | 'secondary'
-	href?: string
-	target?: '_self' | '_blank'
-}
-
-export type { LinkProps }
