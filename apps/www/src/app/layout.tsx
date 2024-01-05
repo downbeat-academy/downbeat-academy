6520d4aee68fae8a8f26198d0bc5cdbc74e0de7a
Jory Tindall <jory.tindall@hashicorp.com>
2024-01-04 23:33:03 -0800

    Removed unused imports

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -1,7 +1,5 @@
 import Fathom from '@lib/fathom'
 import { AppFrame } from '@components/app-frame'
-import { Footer, HeaderNavigation } from '@components/navigation'
-import { ContentWrapper, Content } from '@components/content-wrapper'
 import '@styles/index.scss'
 
 export default function RootLayout({
