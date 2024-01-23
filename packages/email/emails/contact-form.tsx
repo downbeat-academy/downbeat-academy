6f50cc7280e28bfb2201f0421f00f2179307b9b3
Jory Tindall <jory.tindall@hashicorp.com>
2024-01-22 22:04:35 -0800

    Adding react-email, still need to config it a bit

---
+++ packages/email/emails/contact-form.tsx
@@ -0,0 +1,12 @@
+import { Button, Html } from '@react-email/components'
+import * as React from 'react'
+
+export const ContactFormEmail = () => {
+  return (
+    <Html>
+      <Button>Test button</Button>
+    </Html>
+  )
+}
+
+export default ContactFormEmail
