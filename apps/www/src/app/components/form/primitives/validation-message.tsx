e0b17bf66e4729a78fd557e4be7c9780919195a5
Jory Tindall <jory.tindall@hashicorp.com>
2024-01-26 13:02:28 -0800

    Adding icons for validation message

--- apps/www/src/app/components/form/primitives/validation-message.tsx
+++ apps/www/src/app/components/form/primitives/validation-message.tsx
@@ -1,4 +1,7 @@
 import classnames from 'classnames'
+import {
+  CheckCircleOutline,
+} from 'cadence-icons'
 import s from './validation-message.module.scss'
 
 import type { ValidationMessageProps } from "../types"
@@ -15,10 +18,20 @@ const ValidationMessage = ({
     className,
   )
 
+  const getType = (type) => {
+    switch (type) {
+      case 'success':
+        return <CheckCircleOutline width='16' />
+      default:
+        return null
+    }
+  }
+
   return (
     <span
       className={classes}
     >
+      {getType(type)}
       {children}
     </span>
   )
