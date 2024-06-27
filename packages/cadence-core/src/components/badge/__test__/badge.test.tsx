2df25f4ec6e59ffab82300bb99dd704ff3a75a4f
Jory Tindall <jory.tindall@hashicorp.com>
2024-06-26 22:36:54 -0700

    Updating the vitest config

--- packages/cadence-core/src/components/badge/__test__/badge.test.tsx
+++ packages/cadence-core/src/components/badge/__test__/badge.test.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { describe, expect, it } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen} from "@testing-library/react";
 import { Badge } from "../badge";
 
 describe("Badge component", () => {
   it("Badge should render correctly", () => {
     render(<Badge text="Badge" />);
     const badge = screen.getByText("Badge")
-    expect(badge).toBeInTheDocument();
+    expect(badge).toBeInstanceOf(HTMLElement);
   });
 });
