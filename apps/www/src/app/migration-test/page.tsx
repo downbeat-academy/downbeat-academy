f32e678331fbed7d94fee329e9bcf2bef650d1e0
Jory Tindall <jory.tindall@hashicorp.com>
2024-07-07 15:48:34 -0700

    Removed testing pages

--- apps/www/src/app/migration-test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Badge } from 'cadence-core'
-
-export default function MigrationTest() {
-  return (
-    <>
-      <Badge text='Hello World' />
-    </>
-  )
-}
--- packages/cadence-core/rollup.config.js
+++ packages/cadence-core/rollup.config.js
@@ -18,18 +18,18 @@ const globals = {
 };
 
 // Adjust this path to point to your generated tokens CSS file
-const tokensPath = resolve(__dirname, '../your-tokens-package/dist/tokens.css');
+const tokensPath = resolve(__dirname, '../cadence-tokens/dist/tokens.css');
 
 export default {
-  input: ['./src/index.ts'],
+  input: './src/index.ts',
   output: [
     {
-      file: './dist/index.esm.js',
+      file: 'dist/index.esm.js',
       format: 'esm',
       globals,
     },
     {
-      file: './dist/index.cjs.js',
+      file: 'dist/index.cjs.js',
       format: 'cjs',
       globals,
     },
@@ -38,80 +38,23 @@ export default {
     peerDepsExternal(),
     nodeResolve({ extensions, browser: true }),
     commonjs(),
-    typescript(),
+    typescript({ 
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      // declarationDir: 'dist',
+      rootDir: './src'
+    }),
     postcss({
       plugins: [
         postcssPresetEnv(),
         autoprefixer(),
       ],
-      extract: 'dist/cadence-core.min.css',
+      extract: 'cadence-core.min.css',
       minimize: true,
       sourceMap: false,
       include: [tokensPath, '**/*.css'], // Include both tokens and your CSS files
       inject: false, // This prevents automatic injection of styles into the head
     }),
   ],
+  external: ['react', 'react-dom'],
 };
-
-// import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-// import nodeResolve from '@rollup/plugin-node-resolve'
-// import commonjs from '@rollup/plugin-commonjs'
-// import typescript from '@rollup/plugin-typescript'
-// import postcss from 'postcss'
-// import postcssPresetEnv from 'postcss-preset-env'
-// import autoprefixer from 'autoprefixer'
-// import scss from 'rollup-plugin-scss'
-
-// const extensions = ['.js', '.jsx', '.ts', '.tsx', '.css']
-// const globals = {
-// 	react: 'React',
-// 	'react-dom': 'ReactDOM',
-// }
-
-// export default {
-// 	input: ['./src/index.ts'],
-// 	output: [
-// 		{
-// 			file: './dist/index.esm.js',
-// 			format: 'esm',
-// 			globals,
-// 		},
-// 		{
-// 			file: './dist/index.cjs.js',
-// 			format: 'cjs',
-// 			globals,
-// 		},
-// 	],
-// 	plugins: [
-// 		peerDepsExternal(),
-// 		nodeResolve({ extensions, browser: true }),
-// 		commonjs(),
-// 		typescript(),
-// 		postcss({
-// 			plugins: [
-// 				postcssPresetEnv(),
-// 				autoprefixer(),
-// 				scss({
-// 					output: 'dist/cadence-core.min.css',
-// 					outputStyle: 'compressed',
-// 				}),
-// 			],
-// 			autoModules: false,
-// 			onlyModules: false,
-// 			// modules: {
-// 			//   generatedScopedName: (name, filename, css) => {
-// 			//     if (filename.includes('global')) {
-// 			//       return name;
-// 			//     }
-// 			//     const hash = stringHash(css).toString(36).substring(0, 5);
-// 			//     return `cadence_${name}_${hash}`;
-// 			//   }
-// 			// },
-// 			extract: 'css/cadence-core.min.css',
-// 			extensions: ['.scss'],
-// 			use: ['scss'],
-// 			minimize: true,
-// 			sourceMap: false,
-// 		}),
-// 	],
-// }
