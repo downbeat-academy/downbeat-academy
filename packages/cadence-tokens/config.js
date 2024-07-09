c0e6d2a213d4a36426d49bd4b6bf88230816cbab
Jory Tindall <jory.tindall@hashicorp.com>
2024-07-08 23:00:18 -0700

    Fixed transform issue

--- packages/cadence-tokens/config.js
+++ packages/cadence-tokens/config.js
@@ -1,20 +1,11 @@
-const { transform } = require('@divriots/style-dictionary-to-figma')
-
 module.exports = {
 	source: [`tokens/**/*.json`],
-	transform: {},
-	format: {
-		figmaTokens: ({ dictionary }) => {
-			const transformedTokens = transform(dictionary.tokens)
-			return JSON.stringify(transformedTokens, null, 2)
-		},
-	},
 	platforms: {
 		web: {
 			transformGroup: 'css',
 			buildPath: './dist/web/',
 			prefix: 'cds',
-			transforms: ['attribute/cti', 'name/cti/kebab', 'color/hex', 'size/rem'],
+			transforms: ['attribute/cti', 'name/kebab', 'color/hex', 'size/rem'],
 			files: [
 				{
 					destination: 'tokens.scss',
@@ -33,15 +24,53 @@ module.exports = {
 				},
 			],
 		},
-		figma: {
-			transformGroup: 'js',
-			buildPath: './dist/figma/',
-			files: [
-				{
-					destination: 'figma-tokens.json',
-					format: 'figmaTokens',
-				},
-			],
-		},
 	},
-}
+};
+
+// const { transform } = require('@divriots/style-dictionary-to-figma')
+
+// module.exports = {
+// 	source: [`tokens/**/*.json`],
+// 	transform: {},
+// 	format: {
+// 		figmaTokens: ({ dictionary }) => {
+// 			const transformedTokens = transform(dictionary.tokens)
+// 			return JSON.stringify(transformedTokens, null, 2)
+// 		},
+// 	},
+// 	platforms: {
+// 		web: {
+// 			transformGroup: 'css',
+// 			buildPath: './dist/web/',
+// 			prefix: 'cds',
+// 			transforms: ['attribute/cti', 'name/cti/kebab', 'color/hex', 'size/rem'],
+// 			files: [
+// 				{
+// 					destination: 'tokens.scss',
+// 					format: 'scss/variables',
+// 					options: {
+// 						showFileHeader: false,
+// 					},
+// 				},
+// 				{
+// 					destination: 'tokens.css',
+// 					format: 'css/variables',
+// 					prefix: 'cds',
+// 					options: {
+// 						showFileHeader: false,
+// 					},
+// 				},
+// 			],
+// 		},
+// 		figma: {
+// 			transformGroup: 'js',
+// 			buildPath: './dist/figma/',
+// 			files: [
+// 				{
+// 					destination: 'figma-tokens.json',
+// 					format: 'figmaTokens',
+// 				},
+// 			],
+// 		},
+// 	},
+// }
