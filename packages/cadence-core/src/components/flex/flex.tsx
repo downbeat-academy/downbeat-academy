3c59581b6dc700417bc317d2aa3240b60bf56e18
Jory Tindall <jory.tindall@hashicorp.com>
2024-07-11 12:15:16 -0700

    Migrated over code from www

---
+++ packages/cadence-core/src/components/flex/flex.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import classnames from 'classnames'
+import s from './flex.module.scss'
+
+import type { FlexProps } from './types'
+
+const Flex = ({
+	children,
+	tag = 'div',
+	gap = 'none',
+	padding = 'none',
+	direction = 'column',
+	className,
+	alignItems,
+	alignContent,
+	justifyItems,
+	justifyContent,
+	background,
+	wrap,
+}: FlexProps) => {
+	const classes = classnames([
+		s.root,
+		s[`gap--${gap}`],
+		s[`padding--${padding}`],
+		s[`direction--${direction}`],
+		s[`align-items--${alignItems}`],
+		s[`align-content--${alignContent}`],
+		s[`justify-items--${justifyItems}`],
+		s[`justify--${justifyContent}`],
+		s[`background--${background}`],
+		s[wrap ? `wrap` : ''],
+		className,
+	])
+
+	const Tag = tag
+
+	return <Tag className={classes}>{children}</Tag>
+}
+
+export { Flex }
+export type { FlexProps }
