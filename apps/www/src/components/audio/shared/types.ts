f4e8f527fe4f7d84d851fcb84c6650bacea95f9c
Jory Tindall <jory.tindall@hashicorp.com>
2024-11-19 21:31:40 -0800

    Fixing null values in audio player and a bunch of small little fixes

--- apps/www/src/components/audio/shared/types.ts
+++ apps/www/src/components/audio/shared/types.ts
@@ -1,31 +1,56 @@
+interface Track {
+	id: string | number
+	title?: string
+	artist?: string
+	url: string
+	[key: string]: any // For any additional track properties
+}
+
 interface PlayerButtonProps {
 	onClick: () => void
 	ariaLabel: string
 	type:
-		| 'play'
-		| 'pause'
-		| 'stop'
-		| 'rewind'
-		| 'fast-forward'
-		| 'previous'
-		| 'next'
-		| 'volume'
-		| 'volume-mute'
-		| 'volume-quiet'
+	| 'play'
+	| 'pause'
+	| 'stop'
+	| 'rewind'
+	| 'fast-forward'
+	| 'previous'
+	| 'next'
+	| 'volume'
+	| 'volume-mute'
+	| 'volume-quiet'
 	size?: 'small' | 'medium' | 'large'
 	className?: string
 }
 
 interface DisplayTrackProps {
-	currentTrack: any
-	audioRef: any
-	setDuration: any
-	progressBarRef: any
-	handleNext: any
+	currentTrack: Track
+	audioRef: React.RefObject<HTMLAudioElement>
+	setDuration: (duration: number) => void
+	progressBarRef: React.RefObject<HTMLInputElement>
+	handleNext: () => void
 	trackIndex: number
-	tracks: any[]
+	tracks: Track[]
 	showTitle?: boolean
 	showArtist?: boolean
 }
 
-export type { PlayerButtonProps, DisplayTrackProps }
+interface ControlsProps {
+	audioRef: React.RefObject<HTMLAudioElement>
+	progressBarRef: React.RefObject<HTMLInputElement>
+	duration: number
+	setTimeProgress: (time: number) => void
+	tracks: Track[]
+	trackIndex: number
+	setTrackIndex: React.Dispatch<React.SetStateAction<number>>
+	setCurrentTrack: (track: Track) => void
+	handleNext: () => void
+}
+
+export type {
+	Track,
+	PlayerButtonProps,
+	DisplayTrackProps,
+	ControlsProps
+}
