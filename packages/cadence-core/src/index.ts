39cc07989b6d77f030a7d132bc7d4814d04ecb51
Jory Tindall <jory.tindall@hashicorp.com>
2025-01-22 23:42:49 -0800

    Removed some unnecessary index files and centralized all of the component exports

--- packages/cadence-core/src/index.ts
+++ packages/cadence-core/src/index.ts
@@ -1,3 +1,33 @@
+// Components
+
+// - Badge
 export { Badge } from './components/badge';
+export type { BadgeProps } from './components/badge';
+
+//- Brand
+export { LogoLockup } from './components/brand/logo-lockup';
+export { LogoSymbol } from './components/brand/logo-symbol';
+export { LogoText } from './components/brand/logo-text';
+export type { LogoProps } from './components/brand/types';
+
+// - Flex
 export { Flex } from './components/flex';
-export { LogoLockup, LogoSymbol, LogoText } from './components/brand';
+export type { FlexProps } from './components/flex';
+
+// - Form
+export { Form } from './components/form/form/form';
+export { FormField } from './components/form/form-field/form-field';
+export { Input } from './components/form/input/input';
+export { HorizontalWrapper } from './components/form/layout-helpers/horizontal-wrapper';
+export { HelperText } from './components/form/primitives/helper-text'
+export { Label } from './components/form/primitives/label'
+export { ValidationMessage } from './components/form/primitives/validation-message'
+export { Switch } from './components/form/switch/switch';
+export { Textarea } from './components/form/textarea/textarea';
+export type { FormProps } from './components/form/form/types';
+export type { FormFieldProps } from './components/form/form-field/types';
+export type { InputProps } from './components/form/input/types';
+export type { HorizontalWrapperProps } from './components/form/layout-helpers/types';
+export type { HelperTextProps, LabelProps, ValidationMessageProps } from './components/form/primitives/types';
+export type { SwitchProps } from './components/form/switch/types';
+export type { TextareaProps } from './components/form/textarea/types';
