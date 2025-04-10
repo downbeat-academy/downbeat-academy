0366a6877d3ed68c3f74f0d5681af8017fc98f52
Jory Tindall <jory@hypecreativestudios.com>
2025-04-10 13:25:21 -0500

    Removed comments

--- apps/www/src/lib/db/drizzle.ts
+++ apps/www/src/lib/db/drizzle.ts
@@ -1,43 +1,4 @@
-import { neon } from '@neondatabase/serverless'
-// import { drizzle } from 'drizzle-orm/neon-http'
 import { drizzle } from 'drizzle-orm/node-postgres'
-
-// export const createDbClient = (connectionString: string | undefined) => {
-// 	// Debug
-// 	console.log('connection string: ', connectionString)
-
-// 	if (!connectionString) {
-// 		throw new Error('Database connection string is not defined')
-// 	}
-// 	const connection = drizzle(connectionString)
-// 	return connection
-// }
-
-// export function createDbClient(connectionString: string | undefined) {
-// 	if (!connectionString) {
-// 		throw new Error('Database connection string is not defined')
-// 	}
-// 	const sql = neon(connectionString)
-// 	return drizzle(sql)
-// }
-
-// // Lazy initialization of database clients
-// let authDbInstance: ReturnType<typeof drizzle> | null = null
-// let cmsDbInstance: ReturnType<typeof drizzle> | null = null
-
-// export function getAuthDb() {
-// 	if (!authDbInstance) {
-// 		authDbInstance = createDbClient(process.env.DATABASE_URL_AUTH)
-// 	}
-// 	return authDbInstance
-// }
-
-// export function getCmsDb() {
-// 	if (!cmsDbInstance) {
-// 		cmsDbInstance = createDbClient(process.env.DATABASE_URL_CMS)
-// 	}
-// 	return cmsDbInstance
-// }
 
 export const authDb = drizzle(process.env.DATABASE_URL_AUTH)
 export const cmsDb = drizzle(process.env.DATABASE_URL_CMS)
