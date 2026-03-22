cf594ac281169923e7455bc800d78bfec4da6646
Jory Tindall <me@jorytindall.com>
2026-03-21 19:54:12 -0700

    Added Vite 6 allowed hosts to Sanity config

--- apps/cms-sanity/sanity.cli.ts
+++ apps/cms-sanity/sanity.cli.ts
@@ -5,4 +5,12 @@ export default defineCliConfig({
 		projectId: 'v5w3t766',
 		dataset: 'production',
 	},
+	vite: {
+		server: {
+			allowedHosts: ['admin.downbeatacademy.com'],
+		},
+		preview: {
+			allowedHosts: ['admin.downbeatacademy.com'],
+		},
+	},
 })
